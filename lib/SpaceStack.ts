6c287738c78bbb32c95a7e318cf5b0a9d18688d1
Matthew Eddy <meddy672@gmail.com>
2022-08-05 15:54:54 -0400

    added api

--- lib/SpaceStack.ts
+++ lib/SpaceStack.ts
@@ -2,8 +2,12 @@ import { Stack, StackProps } from 'aws-cdk-lib';
 import { Code, Function, Runtime } from 'aws-cdk-lib/aws-lambda';
 import { Construct } from 'constructs';
 import { join } from 'path';
+import { LambdaIntegration, RestApi } from 'aws-cdk-lib/aws-apigateway';
 
 export class SpaceStack extends Stack {
+
+  private api = new RestApi(this, 'SpaceApi');
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
@@ -11,6 +15,10 @@ export class SpaceStack extends Stack {
         runtime: Runtime.NODEJS_14_X,
         code: Code.fromAsset(join(__dirname, '..','services','hello')),
         handler: 'hello.handler'
-      })
+      });
+
+      const helloLambdaIntegration = new LambdaIntegration(helloLambda);
+      const helloLambdaResource = this.api.root.addResource('hello');
+      helloLambdaResource.addMethod('GET', helloLambdaIntegration);
   }
 }
